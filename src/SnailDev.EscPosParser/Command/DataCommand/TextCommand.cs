511b615d2d4034ab6981193febab56fd18f29a44
zhaoming <zhaoming@mallcoo.cn>
2017-09-29 18:34:57 +0800

    container interface and text command

---
+++ src/SnailDev.EscPosParser/Command/DataCommand/TextCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnailDev.EscPosParser
+{
+    public class TextCommand : DataCommand, ITextContainer
+    {
+        /// <summary>
+        /// get text content
+        /// </summary>
+        /// <returns></returns>
+        public string GetContent()
+        {
+            return Data.ToString();
+        }
+    }
+}
---
+++ src/SnailDev.EscPosParser/Command/Interface/IImageContainer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnailDev.EscPosParser
+{
+    public interface IImageContainer
+    {
+        /// <summary>
+        /// get width
+        /// </summary>
+        /// <returns></returns>
+        int GetWidth();
+
+        /// <summary>
+        /// get height
+        /// </summary>
+        /// <returns></returns>
+        int GetHeight();
+    }
+}
